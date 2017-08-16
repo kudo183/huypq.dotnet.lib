a8c2f46b12d18d9d65a12de30a48e370aa469f21
kudo183 <kudo183@gmail.com>
2017-08-16 18:59:24 +0700

    - add huypq.wpf.Utils

---
+++ huypq.wpf.Utils/huypq.wpf.Utils/BindingUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace huypq.wpf.Utils
+{
+    public static class BindingUtils
+    {
+        private static readonly DependencyProperty DummyProperty = DependencyProperty.RegisterAttached(
+          "Dummy",
+          typeof(Object),
+          typeof(DependencyObject),
+          new UIPropertyMetadata(null));
+
+        public static Object Eval(Object container, String expression)
+        {
+            Binding binding = new Binding(expression) { Source = container };
+            DependencyObject dummyDO = new DependencyObject();
+            BindingOperations.SetBinding(dummyDO, DummyProperty, binding);
+            return dummyDO.GetValue(DummyProperty);
+        }
+    }
+}
---
+++ huypq.wpf.Utils/huypq.wpf.Utils/SimpleCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace huypq.wpf.Utils
+{
+    public class SimpleCommand : ICommand
+    {
+        //information for debug
+        private string _commandName;
+
+        private readonly Func<bool> _canExecute;
+        private readonly Action _execute;
+
+        public SimpleCommand(string commandName, Action execute, Func<bool> canExecute)
+        {
+            _commandName = commandName;
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public SimpleCommand(string commandName, Action execute)
+        {
+            _commandName = commandName;
+            _canExecute = () => true;
+            _execute = execute;
+        }
+
+        #region Implementation of ICommand
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+                return _canExecute();
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute?.Invoke();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+        #endregion
+    }
+}
