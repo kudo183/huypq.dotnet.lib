c884152d3ad9c31c42faf52c4e5b98c16bbd5630
kudo183 <kudo183@gmail.com>
2017-08-24 17:26:33 +0700

    - add LoggerProviderWithOptions for compatible with with ServiceLocator.

---
+++ huypq.Logging/huypq.Logging/LoggerProviderWithOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace huypq.Logging
+{
+    public class LoggerProviderWithOptions : LoggerProvider
+    {
+        public class Options
+        {
+            public Func<string, LogLevel, bool> Filter { get; set; }
+            public bool IsIncludeScope { get; set; }
+            public ILoggerProcessor Processor { get; set; }
+        }
+
+        public LoggerProviderWithOptions(Func<string, LogLevel, bool> filter, bool isIncludeScope, ILoggerProcessor processor)
+            : base(filter, isIncludeScope, processor)
+        {
+        }
+
+        public LoggerProviderWithOptions(Options options)
+            : this(options.Filter, options.IsIncludeScope, options.Processor)
+        {
+
+        }
+    }
+}
