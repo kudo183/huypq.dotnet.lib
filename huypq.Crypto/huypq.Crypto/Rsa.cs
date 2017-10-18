7973034c006896692507b47e2b4f04a3533f42bb
gaucon <kudo183@gmail.com>
2017-10-18 11:30:54 +0700

    - add code for JWT token.

---
+++ huypq.Crypto/huypq.Crypto/Rsa.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+
+namespace huypq.Crypto
+{
+    public static class Rsa
+    {
+        static HashAlgorithmName hashName = new HashAlgorithmName("SHA256");
+
+        public static byte[] SignSHA256(byte[] data, RSAParameters key)
+        {
+            var rsa = RSA.Create();
+            rsa.ImportParameters(key);
+            return rsa.SignData(data, hashName, RSASignaturePadding.Pkcs1);
+        }
+
+        public static bool VerifySignSHA256(byte[] data, byte[] sign, RSAParameters key)
+        {
+            var rsa = RSA.Create();
+            rsa.ImportParameters(key);
+            return rsa.VerifyData(data, sign, hashName, RSASignaturePadding.Pkcs1);
+        }
+    }
+}
