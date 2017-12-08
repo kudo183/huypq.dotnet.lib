53f93f23781b8e8dc327d8f3734b524f2e206154
gaucon <kudo183@gmail.com>
2017-12-09 00:26:16 +0700

    - add SHA256.cs for huypq.Login.

---
+++ huypq.Crypto/huypq.Crypto/SHA256.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace huypq.Crypto
+{
+    public static class SHA256Utils
+    {
+        public static string ComputeBase64UrlEncodeHash(string text)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var challengeBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+                return Base64UrlEncoder.Encode(challengeBytes);
+            }
+        }
+    }
+}
