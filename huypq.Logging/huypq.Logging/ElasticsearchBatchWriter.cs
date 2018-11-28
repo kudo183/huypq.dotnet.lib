3421a2b5fd85841b3d03da837d20dfa840c86c3d
win10 desktop <kudo183@gmail.com>
2018-11-28 22:03:50 +0700

    - add ElasticsearchBatchWriter and Extension method.

---
+++ huypq.Logging/huypq.Logging/ElasticsearchBatchWriter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace huypq.Logging
+{
+    public class ElasticsearchBatchWriter : ILogBatchWriter
+    {
+        readonly string _url;
+        HttpClient _httpClient;
+        const string Index = "{\"index\":{}}\n";
+
+        public ElasticsearchBatchWriter(string url, string index)
+        {
+            _url = url + "/" + index + "/_doc/_bulk";
+            _httpClient = new HttpClient();
+        }
+
+        public async Task Write(List<LogEntry> logEntries)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var log in logEntries)
+            {
+                sb.Append(Index);
+                sb.Append(log.Message);
+            }
+            var content = new StringContent(sb.ToString(), Encoding.UTF8, "application/json");
+            var result = await _httpClient.PostAsync(_url, content);
+        }
+    }
+}
---
+++ huypq.Logging/huypq.Logging/LoggingLoggingBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace huypq.Logging
+{
+    public static class LoggingLoggingBuilderExtensions
+    {
+        public static ILoggingBuilder AddLogging(this ILoggingBuilder builder, ILoggerProcessor loggerProcessor, LogLevel logLevel)
+        {
+            builder.AddProvider(new LoggerProvider(
+                (category, level) => true,
+                true,
+                loggerProcessor));
+
+            builder.AddFilter<LoggerProvider>(null, logLevel);
+
+            return builder;
+        }
+
+        public static ILoggingBuilder AddLogging(this ILoggingBuilder builder, ILoggerProcessor loggerProcessor, List<Tuple<string, LogLevel>> logLevels)
+        {
+            builder.AddProvider(new LoggerProvider(
+                (category, level) => true,
+                true,
+                loggerProcessor));
+
+            foreach (var item in logLevels)
+            {
+                builder.AddFilter<LoggerProvider>(item.Item1, item.Item2);
+            }
+
+            return builder;
+        }
+    }
+}
